d8b7e1e91cd7a1ca24d2beb578af0fe528fe5fb8
Prateek Kumar <prateek@prateekkumar.in>
2017-02-28 00:24:39 +0530

    Add support for contributions
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- main.go
+++ main.go
@@ -378,6 +378,51 @@ func initialize() {
 	}
 }
 
+func contribute(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	question := r.FormValue("question")
+	option1 := r.FormValue("option1")
+	option2 := r.FormValue("option2")
+	option3 := r.FormValue("option3")
+	option4 := r.FormValue("option4")
+	answer := r.FormValue("answer")
+	topic := r.FormValue("topic")
+	w.Header().Set("Content-Type", "application/json")
+	rows, err := database.Query(fmt.Sprintf("INSERT INTO %s VALUES (0, '%s', '%s', '%s', '%s', '%s', %v)", topic, question, option1, option2, option3, option4, answer))
+	defer rows.Close()
+	if err != nil {
+		data := Result{false, "Error!"}
+		js, _ := json.Marshal(data)
+		w.Write(js)
+		return
+	}
+	rows1, err := database.Query("SELECT * FROM users WHERE username='" + username + "' AND password='"+password+"'")
+	defer rows1.Close()
+	if err != nil {
+		data := Result{false, "Error!"}
+		js, _ := json.Marshal(data)
+		w.Write(js)
+		return
+	}
+	var id, points, games, contributions int
+	for rows1.Next() {
+		rows1.Scan(&id, &username, &password, &points, &games, &contributions)
+	}
+	contributions += 1
+	rows2, err := database.Query(fmt.Sprintf("UPDATE users SET contributions=%v WHERE username=\"%s\"", contributions, username))
+	defer rows2.Close()
+	if err != nil {
+		data := Result{false, "Error!"}
+		js, _ := json.Marshal(data)
+		w.Write(js)
+		return
+	}
+	data := Result{true, "Thanks for contributing"}
+	js, _ := json.Marshal(data)
+	w.Write(js)
+}
+
 func main() {
 	waiting = make(map[string][]*Player)
 
@@ -393,6 +438,7 @@ func main() {
 		http.HandleFunc("/login", login)
 		http.HandleFunc("/signup", signup)
 		http.HandleFunc("/details", playerDetails)
+		http.HandleFunc("/contri", contribute)
 		http.HandleFunc("/play", func(w http.ResponseWriter, r *http.Request) {
 			play(w, r, upgrader)
 		})
